804f7600e589e9a9b6c25c85f72cc0f5e377a6c2
divyasushinanay <divyasushin.aitrich@gmail.com>
2024-03-28 11:06:17 +0530

    profilepiccompleted

--- pfm/src/app/home/components/profile/profile.component.ts
+++ pfm/src/app/home/components/profile/profile.component.ts
@@ -1,236 +1,13 @@
-import { User } from './../../models/User';
-// import { Component,OnInit} from '@angular/core';
-// import { FormBuilder, FormGroup } from '@angular/forms';
-// import { UserService } from '../../services/user.service';
-// import {ProfileService} from '../../services/profile.service'
-
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent implements OnInit{
-//   myForm!: FormGroup;
-//   userData: any; // Variable to store user details
-//   editMode: boolean = false; // Flag to enable/disable edit mode
-
-//   constructor(private fb: FormBuilder, private userService: UserService, private profileservice:ProfileService) { }
-
-//   ngOnInit(): void {
-//     this.myForm = this.fb.group({
-//       _id:[''],
-//      firstname:[''],
-//      lastname:['']
-      
-//     });
-
-//     this.getuserdetails();
-//   }
-
-//   getuserdetails(){
-//     const id = `65fcfde89b5da09acbaff243`;
-//     this.userService.getuser(id).subscribe((data)=>{
-//       this.myForm.patchValue(data)
-//     })
-//   }
-
-
-//   onsubmit(){
-//     if (this.myForm.valid) {
-//       const updatedvalue = this.myForm.value
-//       this.userService.edituser(updatedvalue).subscribe((res)=>{
-//         if (res) {
-//           if(this.myForm.value){
-//             alert("updated")
-//             console.log(res);
-//           }
-//         }
-//       })
-//     }
-//   }
-
-//   uploadprofilepic(){};
-
-
-//   getprofilepic(){};
-
-// }
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup } from '@angular/forms';
-// import { UserService } from '../../services/user.service';
-// import { ProfileService } from '../../services/profile.service';
-
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent implements OnInit {
-//   myForm!: FormGroup;
-//   profileImage: any; // Variable to store profile image
-//   selectedFile: File | undefined;
-
-//   constructor(private fb: FormBuilder, private userService: UserService, private profileService: ProfileService) { }
-
-//   ngOnInit(): void {
-//     this.myForm = this.fb.group({
-//       firstname: [''],
-//       lastname: ['']
-//     });
-
-//     this.getuserdetails();
-//     this.getProfilePic(); // Fetch profile picture when component initializes
-//   }
-
-//   getuserdetails() {
-//     const id = `65fab73f1c860ff5e300098f`;
-//     this.userService.getuser(id).subscribe((data) => {
-//       this.myForm.patchValue(data);
-//     });
-//   }
-
-//   onsubmit() {
-//     if (this.myForm.valid) {
-//       const updatedValue = this.myForm.value;
-//       this.userService.edituser(updatedValue).subscribe((res) => {
-//         if (res) {
-//           if (this.myForm.value) {
-//             alert("updated");
-//             console.log(res);
-//           }
-//         }
-//       });
-//     }
-//   }
-
-//   onFileSelected(event: any) {
-//     this.selectedFile = event.target.files[0];
-//   }
-
-//   uploadProfilePic() {
-//     if (!this.selectedFile) {
-//       alert("Please select a file.");
-//       return;
-//     }
-
-//     const id = '65fab73f1c860ff5e300098f'; // Assuming this is the user's ID
-//     const formData: FormData = new FormData();
-//     formData.append('file', this.selectedFile);
-
-//     this.profileService.uploaprofilepic(id, formData).subscribe((res) => {
-//       // Handle response as needed
-//       console.log(res);
-//       this.getProfilePic(); // Fetch updated profile picture after upload
-//     });
-//   }
-
-//   getProfilePic() {
-//     const id = '65fab73f1c860ff5e300098f'; // Assuming this is the user's ID
-//     this.profileService.getprofilepic(id).subscribe((res) => {
-//       // Assuming the response contains the image URL in res.profilePic
-//       this.profileImage = res.profilePic;
-//     });
-//   }
-// }
-
-// import { Component, OnInit } from '@angular/core';
-// import { FormBuilder, FormGroup } from '@angular/forms';
-// import { ProfileService } from '../../services/profile.service';
-// import { UserService } from '../../services/user.service';
-// import { Profile } from '../../models/Profile';
-
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent implements OnInit {
 
 
 
-//   myForm!: FormGroup ;
-//   profileImage: string | undefined;
-//   selectedFile: File|any ;
-// imageUrls: any;
-
-
-//   constructor(private fb: FormBuilder, private userService: UserService, private profileService: ProfileService) { }
-
-//   ngOnInit(): void {
-//     this.myForm = this.fb.group({
-//       firstname: [''],
-//       lastname: ['']
-//     });
-//     this.getuserdetails();
-//     this.getProfilePic();
-//   }
- 
-//   getuserdetails() {
-//         const id = `65fab73f1c860ff5e300098f`;
-//         this.userService.getuser(id).subscribe((data) => {
-//           this.myForm.patchValue(data);
-//         });
-//       }
-
-//       onsubmit() {
-//             if (this.myForm.valid) {
-//               const updatedValue = this.myForm.value;
-//               this.userService.edituser(updatedValue).subscribe((res) => {
-//                 if (res) {
-//                   if (this.myForm.value) {
-//                     alert("updated");
-//                     console.log(res);
-//                   }
-//                 }
-//               });
-//             }
-//           }
-        
-
-//           clearForm() {
-//             throw new Error('Method not implemented.');
-//             }
-            
-            
-//             upload($event: Event) {
-//             throw new Error('Method not implemented.');
-//             }
-
-
-//   onFileSelected(event: any) {
-//     this.selectedFile = event.target.files[0];
-//   }
-
-//   uploadProfilePic() {
-//     if (!this.selectedFile) {
-//       alert("Please select a file.");
-//       return;
-//     }
-
-//     const userId = '65fab73f1c860ff5e300098f'; // Replace with actual user ID
-//     this.profileService.uploadProfilePic(userId, this.selectedFile).subscribe((res) => {
-//       console.log('Profile picture uploaded successfully');
-//       this.getProfilePic();
-//     });
-//   }
-
-//   getProfilePic() {
-//     const userId = '65fab73f1c860ff5e300098f'; // Replace with actual user ID
-//     this.profileService.getProfilePic(userId).subscribe((res) => {
-//       console.log(res);
-//       this.profileImage = res.profilePic; // Assuming the response contains the profile picture URL
-//     });
-//   }
-// }
-
-
+// import { User } from './../../models/User';
 // import { Component, OnInit } from '@angular/core';
 // import { FormBuilder, FormGroup } from '@angular/forms';
 // import { UserService } from '../../services/user.service';
 // import { ProfileService } from '../../services/profile.service';
 // import { Profile } from '../../models/Profile';
-
+// // import { User } from '../../models/User';
 
 // @Component({
 //   selector: 'app-profile',
@@ -238,24 +15,24 @@ import { User } from './../../models/User';
 //   styleUrls: ['./profile.component.css']
 // })
 // export class ProfileComponent implements OnInit {
-// picform!: any;
-// clearForm() {
-// throw new Error('Method not implemented.');
-// }
-//   myForm!: FormGroup;
-//   profileImage: string | undefined;
+//   detailsForm!: FormGroup;
+//   picForm!: FormGroup;
+//   imageUrls: string | undefined;
 //   selectedFile: File | undefined;
 //   userId = '65fab73f1c860ff5e300098f'; // Replace with actual user ID
 //   profile: Profile | any;
-//   user:User|any;
+//   user: User | any;
 
 //   constructor(private fb: FormBuilder, private userService: UserService, private profileService: ProfileService) { }
 
 //   ngOnInit(): void {
-//     this.myForm = this.fb.group({
+//     this.detailsForm = this.fb.group({
 //       firstname: [''],
 //       lastname: [''],
-      
+//     });
+
+//     this.picForm = this.fb.group({
+//       imageUrls: ['']
 //     });
 
 //     this.getuserdetails();
@@ -264,17 +41,16 @@ import { User } from './../../models/User';
 
 //   getuserdetails() {
 //     this.userService.getuser(this.userId).subscribe((data) => {
-//       this.myForm.patchValue(data);
+//       this.detailsForm.patchValue(data);
 //     });
 //   }
 
-//   onsubmit() {
-//     if (this.myForm.valid) {
-//       const updatedValue = this.myForm.value;
+//   submitDetails() {
+//     if (this.detailsForm.valid) {
+//       const updatedValue = this.detailsForm.value;
 //       this.userService.edituser(updatedValue).subscribe((res) => {
 //         if (res) {
-//           alert("Updated");
-          
+//           alert("Profile details updated");
 //           this.user = res;
 //           console.log(res);
 //         }
@@ -282,11 +58,7 @@ import { User } from './../../models/User';
 //     }
 //   }
 
-//   onFileSelected(event: any) {
-//     this.selectedFile = event.target.files[0];
-//   }
-
-//   uploadProfilePic() {
+//   submitPic() {
 //     if (!this.selectedFile) {
 //       alert("Please select a file.");
 //       return;
@@ -295,14 +67,102 @@ import { User } from './../../models/User';
 //     this.profileService.uploadProfilePic(this.userId, this.selectedFile).subscribe((res) => {
 //       console.log('Profile picture uploaded successfully');
 //       this.getProfilePic();
+//       this.profile=res
 //     });
 //   }
 
 //   getProfilePic() {
 //     this.profileService.getProfilePic(this.userId).subscribe((res) => {
 //       console.log(res);
-//       this.profileImage = res.profilePic; // Assuming the response contains the profile picture URL
+//       this. imageUrls = res.profilePic; // Assuming the response contains the profile picture URL
 //     });
+//   }
+
+//   onFileSelected(event: any) {
+//     this.selectedFile = event.target.files[0];
+//   }
+// }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup } from '@angular/forms';
+// import { UserService } from '../../services/user.service';
+// import { ProfileService } from '../../services/profile.service';
+// import { Profile } from '../../models/Profile';
+// import { User } from '../../models/User';
+
+// @Component({
+//   selector: 'app-profile',
+//   templateUrl: './profile.component.html',
+//   styleUrls: ['./profile.component.css']
+// })
+// export class ProfileComponent implements OnInit {
+//   detailsForm!: FormGroup;
+//   picForm!: FormGroup;
+//   imageUrls: string | undefined;
+//   selectedFile: File | undefined;
+//   userId = '65fab73f1c860ff5e300098f'; // Replace with actual user ID
+//   profile: Profile | any;
+//   user: User | any;
+
+//   constructor(private fb: FormBuilder, private userService: UserService, private profileService: ProfileService) { }
+
+//   ngOnInit(): void {
+//     this.detailsForm = this.fb.group({
+//       firstname: [''],
+//       lastname: [''],
+//     });
+
+//     this.picForm = this.fb.group({
+//       imageUrls: ['']
+//     });
+
+//     this.getuserdetails();
+//     this.getProfilePic();
+//   }
+
+//   getuserdetails() {
+//     this.userService.getuser(this.userId).subscribe((data) => {
+//       this.detailsForm.patchValue(data);
+//     });
+//   }
+
+//   submitDetails() {
+//     if (this.detailsForm.valid) {
+//       const updatedValue = this.detailsForm.value;
+//       this.userService.edituser(updatedValue).subscribe((res) => {
+//         if (res) {
+//           alert("Profile details updated");
+//           this.user = res;
+//           console.log(res);
+//         }
+//       });
+//     }
+//   }
+
+//   submitPic() {
+//     if (!this.selectedFile) {
+//       alert("Please select a file.");
+//       return;
+//     }
+
+//     this.profileService.uploadProfilePic(this.userId, this.selectedFile).subscribe((res) => {
+//       console.log('Profile picture uploaded successfully');
+//       this.getProfilePic();
+//       this.profile=res
+//     });
+//   }
+
+//   getProfilePic() {
+//     this.profileService.getProfilePic(this.userId).subscribe((res) => {
+//       console.log(res);
+//       this.imageUrls = res.imageUrl; // Assuming the response contains the profile picture URL
+//     });
+//   }
+
+//   onFileSelected(event: any) {
+//     this.selectedFile = event.target.files[0];
 //   }
 // }
 
@@ -311,7 +171,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { ProfileService } from '../../services/profile.service';
 import { Profile } from '../../models/Profile';
-// import { User } from '../../models/User';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-profile',
@@ -321,7 +181,7 @@ import { Profile } from '../../models/Profile';
 export class ProfileComponent implements OnInit {
   detailsForm!: FormGroup;
   picForm!: FormGroup;
-  profileImage: string | undefined;
+  imageUrls: string | undefined;
   selectedFile: File | undefined;
   userId = '65fab73f1c860ff5e300098f'; // Replace with actual user ID
   profile: Profile | any;
@@ -336,14 +196,14 @@ export class ProfileComponent implements OnInit {
     });
 
     this.picForm = this.fb.group({
-      profilePic: ['']
+      ProfilePicture: ['']
     });
 
-    this.getuserdetails();
+    this.getUserDetails();
     this.getProfilePic();
   }
 
-  getuserdetails() {
+  getUserDetails() {
     this.userService.getuser(this.userId).subscribe((data) => {
       this.detailsForm.patchValue(data);
     });
@@ -370,16 +230,40 @@ export class ProfileComponent implements OnInit {
 
     this.profileService.uploadProfilePic(this.userId, this.selectedFile).subscribe((res) => {
       console.log('Profile picture uploaded successfully');
+      alert("photo uploaded")
       this.getProfilePic();
+      this.profile = res;
     });
   }
+
+  // getProfilePic() {
+  //   this.profileService.getProfilePic(this.userId).subscribe((res) => {
+  //     console.log(res);
+  //     // this.profile = res;
+  //     this.imageUrls = res.ProfilePicture; // Assuming the response contains the profile picture URL
+  //    console.log(this.imageUrls)
+  //   });
+  // }
+
+  // getProfilePic() {
+  //   this.profileService.getProfilePic(this.userId).subscribe((res) => {
+  //     console.log(res);
+  //     this.imageUrls = res.ProfilePicture; // Assuming the response contains the profile picture URL
+  //     console.log(this.imageUrls)
+  //   });
+  // }
+  
+
 
   getProfilePic() {
     this.profileService.getProfilePic(this.userId).subscribe((res) => {
       console.log(res);
-      this.profileImage = res.profilePic; // Assuming the response contains the profile picture URL
+      this.imageUrls = res.imageUrl; // Assuming the response contains the profile picture URL
+      console.log(this.imageUrls)
     });
   }
+  
+
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
--- pfm/src/app/home/services/profile.service.ts
+++ pfm/src/app/home/services/profile.service.ts
@@ -1,3 +1,5 @@
+
+
 // import { Injectable } from '@angular/core';
 // import { Profile } from '../models/Profile';
 // import { HttpClient } from '@angular/common/http';
@@ -10,26 +12,24 @@
 //   providedIn: 'root'
 // })
 // export class ProfileService {
-
-//   constructor(private http:HttpClient) { }
+//   constructor(private http: HttpClient) { }
 //   private apiUrl = environment.apiEndPoint;
 
-//   uploaprofilepic(id: string, data: FormData): Observable<any> {
-//     return this.http.post<any>(`${this.apiUrl}api/v1/profile/user/${id}/profilepic`, data);
+
+//   uploadProfilePic(_userId: string, file: File): Observable<any> {
+//     const formData: FormData = new FormData();
+//     formData.append('file', file);
+//     return this.http.post<any>(`${this.apiUrl}api/v1/profile/user/65fab73f1c860ff5e300098f`, formData); // Update URL construction
 //   }
 
-//   getprofilepic(id: string): Observable<any> {
-//     return this.http.get<any>(`${this.apiUrl}api/v1/profile/user/${id}/profilepic`);
+//   getProfilePic(_userId: string): Observable<any> {
+//     return this.http.get<any>(`${this.apiUrl}/api/v1/profile/user/65fab73f1c860ff5e300098f`); // Update URL construction
 //   }
 // }
 
-
 import { Injectable } from '@angular/core';
-import { Profile } from '../models/Profile';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
-import { User } from '../models/User';
-
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -39,14 +39,14 @@ export class ProfileService {
   constructor(private http: HttpClient) { }
   private apiUrl = environment.apiEndPoint;
 
-
   uploadProfilePic(userId: string, file: File): Observable<any> {
     const formData: FormData = new FormData();
-    formData.append('file', file);
-    return this.http.post<any>(`${this.apiUrl}api/v1/profile/user/65fab73f1c860ff5e300098f`, formData); // Update URL construction
+    formData.append('ProfilePicture', file);
+    return this.http.post<any>(`${this.apiUrl}/api/v1/profile/user/${userId}`, formData);
   }
 
   getProfilePic(userId: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}api/v1/profile/user/65fab73f1c860ff5e300098f`); // Update URL construction
+    return this.http.get<any>(`${this.apiUrl}/api/v1/profile/user/${userId}`);
   }
 }
+
